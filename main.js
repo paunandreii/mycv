6b9198ce7ceb5d92840a35f26397259dc6ff4ceb
BillyButcher95 <andrei.paun95@yahoo.com>
2023-01-11 02:13:25 +0200

    remove pages change colors

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
+function show(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function displayHome() {
+  hideAllPages();
+  show("home");
+}
+function displaySkills() {
+  hideAllPages();
+  show("skills");
+}
+function displayLanguages() {
+  hideAllPages();
+  show("languages");
+}
+function displayProjects() {
+  hideAllPages();
+  show("projects");
+}
+
+displaySkills();
